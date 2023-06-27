c1ca4ead8ac2dafe2d674ba0dade521a50f8778a
Ramkh05ho <137823096+Ramkh05ho@users.noreply.github.com>
2023-06-27 09:57:17 -0500

    Update RamiLab1.ino

--- src/RamiLab1.ino
+++ src/RamiLab1.ino
@@ -1,17 +1,14 @@
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
 
+void setup() {
+  pinMode(D5,OUTPUT);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
-pinMode(D5,OUTPUT);
-delay(1000);
-digitalWrite(D5, HIGH);
-delay(1000);
-digitalWrite(D5,LOW);
+  delay(1000);
+  digitalWrite(D5, HIGH);
+  delay(1000);
+  digitalWrite(D5,LOW);
 
 }
